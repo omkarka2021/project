48ee9768bf043b88292fa0781b882900e7317922
omkarka2021 <138841707+omkarka2021@users.noreply.github.com>
2023-07-07 08:54:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// const express = require ("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+
+
+// app.post("/",function(req,res){
+//   function myFunction(){
+//       document.querySelector(".btn").addEventListener("click",function(){
+//       res.sendFile(__dirname + "/home.html");
+//     })}
+// })
+
+// app.get("/index",function(request,response){
+//   response.sendFile(__dirname+"/index.html");
+// });
+
+// app.listen(3000,function(){
+//   console.log("started at port 3000");
+// });
+
+function myFunction(){
+  window.open("index.html");
+}
